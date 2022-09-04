063ee2bdf5aaecfa2ee64ebe67599bcdf23e070e
Nelson belgrade <nelson.belgrade@externe.arval.com>
2022-09-04 22:51:17 +0200

    Create model

--- src/app/user/states/user/user.action.ts
+++ src/app/user/states/user/user.action.ts
@@ -1,15 +1,17 @@
+import { UserModel } from "./user.model";
+
 export class GetUsers {
     static readonly type = '[Users] Fetch';
 }
 
 export class AddUsers {
     static readonly type = '[Users] Add';
-    constructor(public userData: any) { }
+    constructor(public userData: UserModel) { }
 }
 
 export class UpdateUsers {
     static readonly type = '[Users] Update';
-    constructor(public userData: any, public userId: number) { }
+    constructor(public userData: UserModel, public userId: number) { }
 }
 
 export class DeleteUsers {
